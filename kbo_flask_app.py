540cb005cbc35bd638c7e5481ecb51ed19621480
JY <pyeonjy97@gmail.com>
2018-01-07 00:19:48 +0900

    팀명 대소문자 & 폼 변경

--- kbo_flask_app.py
+++ kbo_flask_app.py
@@ -11,6 +11,12 @@ app.config['MONGO_URI'] = os.environ['kbo_mlab_uri']
 mongo = PyMongo(app)
 env = Environment(loader=FileSystemLoader('templates'))
 
+def teamname_case_insensitive(team):
+	if team == 'KT':
+		return 'kt'
+	elif team != 'kt':
+		return team.upper()
+
 @app.route('/')
 def index_page():
 	return env.get_template('index.html').render()
@@ -18,7 +24,7 @@ def index_page():
 @app.route('/search')
 def search_page():
 	date = request.args.get('date')
-	team = request.args.get('team')
+	team = teamname_case_insensitive(request.args.get('team'))
 	bson_result = mongo.db.erm.find({'date': date, 'team': team}, {'_id': False})
 	result = dumps(bson_result, ensure_ascii=False)
 	return env.get_template('search.html').render(result=result)
